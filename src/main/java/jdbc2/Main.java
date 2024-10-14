8511126db855750a5f034732b18d75684b6b5906
AlinaSergeeva6 <43118734+AlinaSergeeva6@users.noreply.github.com>
2024-10-14 22:52:37 +0300

    Update Main.java

--- src/main/java/jdbc2/Main.java
+++ src/main/java/jdbc2/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
     static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/myorders";
     static final String DB_USER = "root";
-    static final String DB_PASSWORD = "@lina0661885624";
+    static final String DB_PASSWORD = "1111";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
